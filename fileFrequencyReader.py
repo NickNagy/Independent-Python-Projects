aae8119894fadde7c921f3c6868e0132ce1a6aad
NickNagy <33533928+NickNagy@users.noreply.github.com>
2018-05-27 16:51:54 -0700

    Update fileFrequencyReader.py

--- fileFrequencyReader.py
+++ fileFrequencyReader.py
@@ -1,11 +1,41 @@
 import re
-from scipy.fftpack import fft, rfft
+from scipy.fftpack import fft, fftfreq
 import tensorflow as tf
 
 FILE_NAME = '440Hz.dat'
 READS_PER_SEC = 1
 
 fileFreqSequence = [] # to use for sequential frequency storage
+
+# modified FFT method from:
+# https://stackoverflow.com/questions/50556705/accurate-method-for-most-prominent-frequencies-of-an-audio-signal/50557340#50557340
+def fft_ups(data, rate, upsampling_factor=100):
+    """fft with upsampling
+
+    allows precise fft analysis by lengthening the
+    target length.
+    data:
+        the data on which fft is performed
+    upsampling_factor[opt]:
+        the factor by which the data length is multiplied
+    returns tuple (frequencies, amplitudes). amps are coeffed. mirror
+        data is cut
+    """
+    def get_target():
+        "as numpy's fft works best with powers of 2, find the smallest"
+        "power near upsampling_factor"""
+        for i in range(50):
+            if 2**i > len(data)*upsampling_factor:
+                return 2**(i-1)
+
+    "coeff corrects amplitude"
+    coeff = 2/len(data)
+    target_length = get_target()
+
+    "the half-length trick is to get rid of mirrored data"
+    xf = fftfreq(target_length, 1/rate)[:target_length//2]
+    yf = coeff*fft(data, target_length)[:target_length//2]
+    return xf, yf
 
 soundFile = open(FILE_NAME) # text file of WAV file data
 
@@ -32,10 +62,8 @@ index = 0
 
 # store frequencies in fileFreqSequence while reading through file
 while index < len(dataList):
-    rawFTData = fft(dataList[index:index+(fs*READS_PER_SEC)])
-    # TODO get accurate frequency from rawFTData
-    # TODO remove data points outside of human hearing range
-    freqProminent = rawFTData[0]
+    freq, amp = fft_ups(dataList[index:index+(fs*READS_PER_SEC)], fs)
+    freqProminent = freq[amp.index(max(amp))] # freq[index of max amplitude]
     fileFreqSequence.apppend(freqProminent)
     index += fs*READS_PER_SEC
 
