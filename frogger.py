8c67b1d81acf2053008dddd2408ffeb12eb0a3b7
NickNagy <33533928+NickNagy@users.noreply.github.com>
2018-09-18 00:47:25 -0700

    Update frogger.py

--- frogger.py
+++ frogger.py
@@ -1,94 +1,160 @@
 import numpy as np
-import copy
 import random
+import copy
 from matplotlib import pyplot as plt
 
+CAR = -100
+WIN = 200
 
-class Frogger:
-    def __init__(self, nrows, ncols, is_win=False):
-        self.nrows = nrows
-        self.ncols = ncols
-        self.is_win = is_win
-        self.frog = 50
-        self.car = 255
-        self.nmoves = 0
-        self.time = 0
-        self.attempts = []
-        self.location = (self.nrows - 1, int(self.ncols / 2))
-        self.grid = np.zeros((self.nrows, self.ncols))
-        # set cars...
-        for row in range(1, self.nrows - 1):  # exclude bottom & top row
-            self.grid[row][random.randint(0, self.ncols - 1)] = self.car
+class QLearn:
+    """params
+        grid: np.array, map of environment/states
+        actions: list, actions agent can take in each state
+            assumes actions>=4, and first four indeces: [up,down,left,right,...]
+        start: tuple, starting position
+        states: dictionary of all states in the environment and their corresponding Q-states
+    """
 
-    def update_grid(self):
-        # TODO: evaluate if frog has landed on a car space
-        # TODO: don't want frog to roll w/ cars during updates -- don't actually place in grid
-        if self.grid[self.location[0]][self.location[1]] == self.car:
-            self.attempts.append((self.time, self.nmoves))
-            self.time = 0
-            self.nmoves = 0
-            self.location = (self.nrows - 1, int(self.ncols / 2))
-        self.time += 1
-        for row in range(1, self.nrows - 1):
+    def __init__(self, environment, actions, states, start):
+        self.memorygrid = 255*np.ones(environment.get_shape())
+        self.environment = environment
+        self.grid_shape = environment.get_shape()
+        self.actions = actions
+        self.moves = 0
+        self.attempts = 0
+        self.wins = 0
+        self.location = self.start = start
+        self.states = states
+        #self.table = np.ndarray(shape=(self.grid_shape[0],self.grid_shape[1],len(self.actions)))
+        #for i in range(self.grid_shape[0]):
+        #    for j in range(self.grid_shape[1]):
+        #        self.table[i][j] = len(self.actions) * [0]
+        # TODO: check what typical values are for alpha and gamma
+        self.state = self.get_state(self.location)
+        self.alpha = 0.9
+        self.gamma = 0.9
+        self.epsilon = 1
+
+    def get_state_key(self, location):
+        key = ""
+        grid = self.environment.get_grid()
+        key+=str(int(grid[location[0]-1][location[1]-1] < 0))
+        key+=str(int(grid[location[0] - 1][location[1]] < 0))
+        key+=str(int(grid[location[0] - 1][(location[1] + 1)%self.grid_shape[1]] < 0))
+        key+=str(int(grid[location[0]][location[1] - 1] < 0))
+        key+=str(int(grid[location[0]][(location[1] + 1)%self.grid_shape[1]] < 0))
+        key+=str(int(grid[(location[0]+1)%self.grid_shape[0]][location[1] - 1] < 0))
+        key+=str(int(grid[(location[0]+1)%self.grid_shape[0]][location[1]] < 0))
+        key+=str(int(grid[(location[0]+1)%self.grid_shape[0]][(location[1] + 1)%self.grid_shape[1]] < 0))
+        return key
+
+    def get_state(self, location):
+        key = self.get_state_key(location)
+        #print("STATE: " + str(self.states[key]))
+        return copy.deepcopy(self.states[key])
+        #return copy.deepcopy(self.table[location[0]][location[1]])
+
+    def can_move(self, action):
+        # if self.is_win: return False
+        if action == 0 and self.location[0] <= 0: return False
+        if action == 1 and self.location[0] >= self.grid_shape[0] - 1: return False
+        if action == 2 and self.location[1] <= 0: return False
+        if action == 3 and self.location[1] >= self.grid_shape[1] - 1: return False
+        return True
+
+    def move(self, action):
+        # self.moves += 1
+        if action == 0:
+            return (self.location[0] - 1, self.location[1])
+        elif action == 1:
+            return (self.location[0] + 1, self.location[1])
+        elif action == 2:
+            return (self.location[0], self.location[1] - 1)
+        else:
+            return (self.location[0], self.location[1] + 1)
+
+    """params
+        location: tuple, location in state table
+        action:, integer, corresponding to index in actions
+    """
+
+    # TODO: fix -> the way next states are evaluated contradicts grid updates!!
+    def learn(self):
+        fig,ax = plt.subplots()
+        while(True):
+            action = 0
+            self.display(axis=ax)
+            if random.uniform(0, 1.0) > self.epsilon:  # exploitation
+                action = np.argmax(self.get_state(self.location))#[0]
+            else:  # choose random action
+                action = random.randint(0, len(self.actions)-1)
+            #print(self.actions[action])
+            if self.can_move(action):
+                # q function
+                grid = self.environment.get_grid()
+                next_location = self.move(action)
+                next_state = self.get_state(next_location)
+                next_projection = max(next_state)
+                next_reward = grid[next_location[0]][next_location[1]]
+                current_key = self.get_state_key(self.location)
+                self.states[current_key][action] = self.state[action] + self.alpha * (
+                    next_reward + (self.gamma * next_projection) - self.state[action])
+                self.display(axis=ax)
+                self.memorygrid[self.location[0]][self.location[1]] *= 0.9
+                self.location = next_location
+                self.state = next_state
+                self.moves += 1
+                if self.epsilon > 0.1: self.epsilon -= 0.01
+                # update to see if collision with car
+                self.environment.update()
+                if self.environment.get_grid()[self.location[0]][self.location[1]] == CAR:
+                    self.location = self.start
+                    self.state = self.get_state(self.location)
+                    self.attempts += 1
+                    self.moves = 0
+                if self.location[0] == 0:
+                    self.wins += 1
+                    self.attempts += 1
+                    self.moves = 0
+                    self.location = self.start
+                    self.state = self.get_state(self.location)
+        plt.show()
+
+    def display(self, axis):
+        board = self.environment.get_grid()
+        board[self.location[0]][self.location[1]] = 25
+        axis.imshow(board)
+        plt.suptitle("Wins: " + str(self.wins) + "; Attempt: " + str(self.attempts) + "; Moves: " + str(self.moves))# + "; Q-state: " +
+                     #str(self.state))#' '.join(str(e) for e in self.state) + "]")
+        plt.pause(0.001)
+
+class Frogger_Environment:
+
+    def __init__(self, shape):
+        self.grid = np.ones(shape=shape)
+        self.grid[0] *= 200
+        for row in range(1, np.shape(self.grid)[0] - 1):  # exclude bottom & top row
+            self.grid[row][random.randint(0, np.shape(self.grid)[1] - 1)] = -100
+
+    def update(self):
+        for row in range(1, np.shape(self.grid)[0] - 1):
             if row % 2 == 0:
                 self.grid[row] = np.roll(self.grid[row], 1)
             else:
                 self.grid[row] = np.roll(self.grid[row], -1)
 
-    def can_move(self, direction):
-        if self.is_win: return False
-        if direction == 'down' and self.location[0] >= self.nrows - 1: return False
-        if direction == 'left' and self.location[1] <= 0: return False
-        if direction == 'right' and self.location[1] >= self.ncols - 1: return False
-        return True
+    def get_grid(self):
+        return copy.deepcopy(self.grid)
 
-    def move(self, direction):
-        self.nmoves += 1
-        if direction == 'down':
-            self.location[0] += 1
-        elif direction == 'up':
-            self.location[0] -= 1
-        elif direction == 'right':
-            self.location[1] += 1
-        else:
-            self.location[1] -= 1
+    def get_shape(self):
+        return np.shape(self.grid)
 
-    """ for showing frog on board """
+frogger_states = {}
+for i in range(0, 256):
+    frogger_states[str('{0:08b}'.format(i))] = [0,0,0,0]
 
-    def display(self, axis, pause_time):
-        board = copy.deepcopy(self.grid)
-        board[self.location[0]][self.location[1]] = 50
-        axis.imshow(board)
-        axis.set_title("Attempt: " + str(len(self.attempts)) + "; Time: " + str(copy.deepcopy(self.time)) + "; Moves " +
-                       str(copy.deepcopy(self.nmoves)))
-        plt.pause(pause_time)
+#print(frogger_states.keys())
 
-
-""" From CSE 415 """
-
-
-class Operator:
-    def __init__(self, name, precond, state_transf):
-        self.name = name
-        self.precond = precond
-        self.state_transf = state_transf
-
-    def is_applicable(self, s):
-        return self.precond(s)
-
-    def apply(self, s):
-        return self.state_transf(s)
-
-
-movements = ['up', 'down', 'left', 'right']
-OPERATORS = [Operator("Move " + movement,
-                      lambda s, m=movement: s.can_move(m),
-                      lambda s, m=movement: s.move(m))
-             for movement in movements]
-
-frogger = Frogger(nrows=10, ncols=10)
-fig, ax = plt.subplots()
-while True:
-    frogger.update_grid()
-    frogger.display(axis=ax, pause_time=0.05)
-plt.show()
+agent = QLearn(environment=Frogger_Environment(shape=(5,5)), actions=['up', 'down', 'left', 'right'],
+               start=(4, 2), states=frogger_states)
+agent.learn()
