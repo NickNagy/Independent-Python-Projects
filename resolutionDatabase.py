7656eb90a76085a0a83deebd21d1a6702c2ce3d3
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2018-12-22 00:30:33 -0800

    Update resolutionDatabase.py

--- resolutionDatabase.py
+++ resolutionDatabase.py
@@ -3,33 +3,34 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.misc import imresize
+import random
 
-data_path = 'D:\\flickr-image-dataset\\flickr30k_images\\flickr30k_images\\'
+data_path = 'D:\\flickr-image-dataset\\flickr30k_images\\'
+img_folder = data_path + 'flickr30k_images\\'
+training_folder = data_path + 'Training\\'
+validation_folder = data_path + 'Validation\\'
+testing_folder = data_path + 'Testing\\'
 
 resolution = 2
 
-total_width = 0
-total_height = 0
-total_images = 0
+y_dim = 512
+x_dim = int(y_dim/resolution)
 
-images = OrderedDict()
-
-for _, _, files in os.walk(data_path):
+counter = 31783
+for _, _, files in os.walk(img_folder):
     for file in files:
-        total_images += 1
-        print(file)
-        image = Image.open(data_path + file)
-        total_width += np.shape(image)[0]
-        total_height += np.shape(image)[1]
-        images[file] = image
-
-print(total_width / total_images)
-print(total_height / total_images)
-
-avg_width = int(total_width/total_images)
-avg_height = int(total_height/total_images)
-
-for image in images:
-    img_y = imresize(image, (avg_width, avg_height, 3))
-    img_x = imresize(img_y, (np.shape(img_y)[0]/resolution, np.shape(img_y)[1]/resolution, 3))
-    Image.save
+        which_folder = random.randint(0, 10)
+        image = Image.open(img_folder + file)
+        img_y = imresize(image, (y_dim, y_dim, 3))
+        img_x = imresize(image, (x_dim, x_dim, 3))
+        if which_folder < 6:
+            save_folder = training_folder
+        elif which_folder < 8:
+            save_folder = validation_folder
+        else:
+            save_folder = testing_folder
+        np.save(save_folder + file + "_x.npy", img_x)
+        np.save(save_folder + file + "_y.npy", img_y)
+        np.save(save_folder + file + "_w.npy", np.ones(shape=(y_dim, y_dim, 1)))
+        counter -= 1
+        print(str(counter) + ": " + file)
